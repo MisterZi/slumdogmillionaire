eb6e31a4bd6ced1afde26db5898aef5c21466c92
Alex <alex@alex-VirtualBox.(none)>
2016-06-30 20:25:23 +0300

    RSpec game_controller for anon tests

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -27,7 +27,43 @@ RSpec.describe GamesController, type: :controller do
       expect(response).to redirect_to(new_user_session_path) # devise должен отправить на логин
       expect(flash[:alert]).to be # во flash должена быть прописана ошибка
     end
+
+    # не может вызвать create
+    it 'dont create' do
+      post :create, id: game_w_questions.id
+
+      game = assigns(:game)
+      expect(game).to be_nil
+
+      expect(response).to redirect_to(new_user_session_path)
+      expect(flash[:alert]).to be
+    end
+
+    # не может вызвать help
+    it 'dont help' do
+      put :help, id: game_w_questions.id
+
+      expect(response).to redirect_to(new_user_session_path)
+      expect(flash[:alert]).to be
+    end
+
+    # не может вызвать answer
+    it 'dont answer' do
+      put :answer, id: game_w_questions.id
+
+      expect(response).to redirect_to(new_user_session_path)
+      expect(flash[:alert]).to be
+    end
+
+    # не может вызвать take_money
+    it 'dont take_money' do
+      put :take_money, id: game_w_questions.id
+
+      expect(response).to redirect_to(new_user_session_path)
+      expect(flash[:alert]).to be
+    end
   end
+
 
   # группа тестов на экшены контроллера, доступных залогиненным юзерам
   context 'Usual user' do
@@ -110,11 +146,21 @@ RSpec.describe GamesController, type: :controller do
       expect(flash[:warning]).to be
     end
 
+    # юзер пытается создать новую игру, не закончив старую
     it 'try to create second game' do
 
+      # убедились что есть игра в работе
       expect(game_w_questions.finished?).to be_falsey
 
+      # отправляем запрос на создание, убеждаемся что новых Game не создалось
       expect { post :create }.to change(Game, :count).by(0)
+
+      game = assigns(:game) # вытаскиваем из контроллера поле @game
+      expect(game).to be_nil
+
+      # и редирект на страницу старой игры
+      expect(response).to redirect_to(game_path(game_w_questions))
+      expect(flash[:alert]).to be
 
     end
   end
